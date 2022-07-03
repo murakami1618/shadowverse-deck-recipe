282f010eab15d9e542f3ec6f957af1a20f39ad83
suguru-gbgg <takagisn12@gmail.com>
2022-07-03 20:36:08 +0900

    デッキリストを見やすくした

--- resources/views/deck_list.blade.php
+++ resources/views/deck_list.blade.php
@@ -1,17 +1,24 @@
 @extends('layouts.app')
-@section('content')
 
-@foreach($decks as $deck)
-<form action="{{ route('card/search') }}" method="post">
-@csrf
-        <input type="image" src="{{ $deck->leader_card }}">
-        <input type="hidden" name="deckid" value="{{ $deck->id }}">
-        <input type="hidden" name="deckclass" value="{{ $deck->deck_class }}">
-    </form>    
-    </br>
-    <a>{{ $deck->deck_class }}</a>
-    </br>
-    <a>{{ $deck->deck_name }}</a>
-    </br>
-@endforeach
+@section('content')
+<div class="container">
+    <div class="row">
+        @foreach($decks as $deck)
+            <div class="col-3 p-4">
+                <div class="card">
+                    <form action="{{ route('card/search') }}" method="post">
+                        @csrf
+                        <input type="image" src="{{ $deck->leader_card }}" class="card-img-top" alt="leader_card">
+                        <input type="hidden" name="deckid" value="{{ $deck->id }}">
+                        <input type="hidden" name="deckclass" value="{{ $deck->deck_class }}">
+                    </form>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item text-center">{{ $deck->deck_class }}</li>
+                        <li class="list-group-item text-center">{{ $deck->deck_name }}</li>
+                    </ul>
+                </div>
+            </div> 
+        @endforeach
+    </div>
+</div>
 @endsection
